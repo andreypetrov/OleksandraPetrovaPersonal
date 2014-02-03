e9f507a6573d9c3b39a980abe761e4ab9439bc48
Andrey Petrov <rokanor@gmail.com>
2014-02-02 22:08:44 -0500

    changes by sasha

---
+++ public/js/views_backup/home/home.js
@@ -0,0 +1,92 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrey
+ * Date: 2013-09-11
+ * Time: 10:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+define([
+    'jquery',
+    'views/archview',
+    'hbs!views/home/homeTemplate',
+    'nivoslider',
+    'skrollr',
+    'skrollrmenu'
+], function ($, ArchView, template, nivoslider) {
+    return ArchView.extend({
+        model: {adjective: "awesome"},
+
+        template: template,
+        skrollParentEl: 0,
+        sliderEl: 0,
+
+        initialize: function () {
+            ArchView.prototype.initialize.apply(this); //super call
+        },
+
+
+        initDomHandles: function () {
+            this.skrollParentEl = this.$el.find('.skrollr-parent');
+            this.sliderEl = this.$el.find('#slider');
+
+
+        },
+
+
+        render: function () {
+            ArchView.prototype.render.apply(this);
+            //activate the slider
+
+            console.log(this.sliderEl);
+            return this;
+
+        },
+
+
+        /**
+         *  Always call this function after you have added this.el to the dom of the page
+         */
+        postRender:function() {
+            this.initSkrollr();
+            this.initNivoSlider();
+        },
+
+
+        initSkrollr: function() {
+            var s = skrollr.init({
+                forceHeight: false,
+                smoothScrolling: true,
+                edgeStrategy: 'set'
+            });
+            skrollr.menu.init(s, {
+                duration: function(currentTop, targetTop){//the duration of the animation as a function of the distance
+
+                  var distance = Math.abs(targetTop - currentTop);
+                  var speed = 2; // 2000 pixels per second
+
+                  var time = distance / speed; // t = S/v, where
+                  return time;
+                },
+                easing: 'linear'
+            });
+        },
+
+        initNivoSlider: function() {
+
+            $(this.sliderEl).nivoSlider({
+                effect: 'slideInLeft'//,
+                // manualAdvance: true
+            });
+        },
+
+        events: {
+            "click .menu-btn-start": "onStart",
+            "click .menu-btn-settings": "onSettings",
+            "click .menu-btn-about": "onAbout",
+            "click .menu-btn-rules": "onRules"
+
+        }
+
+
+    });
+});
