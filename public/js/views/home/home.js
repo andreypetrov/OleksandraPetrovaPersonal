864febe1d4b90cc48f1ac7816df31369175e0be5
Andrey Petrov <rokanor@gmail.com>
2014-02-09 22:03:26 -0500

    added email regex verification client side

--- public/js/views/home/home.js
+++ public/js/views/home/home.js
@@ -171,26 +171,27 @@ define([
             else return this.contactsNavEl;
         },
 
-        onEmailSubmit: function () {
+        onEmailSubmit: function () {              //TODO add prettier error handling
+            if (this.isEmailValid(this.emailEmailEl.val())) {
+                var data = JSON.stringify(this.getEmailObject());
 
-            var data = JSON.stringify(this.getEmailObject());
-
-            $.ajax({
-                type: "POST",
-                url: "/email",
-                // The key needs to match your method's input parameter (case-sensitive).
-                data: data,
-                contentType: "application/json; charset=utf-8",
-                //dataType: "json",
-                success: function (result) {
-                    alert(result);
-                },
-                failure: function (errMsg) {
-                    alert(errMsg);
-                }
-            });
-
-
+                $.ajax({
+                    type: "POST",
+                    url: "/email",
+                    // The key needs to match your method's input parameter (case-sensitive).
+                    data: data,
+                    contentType: "application/json; charset=utf-8",
+                    //dataType: "json",
+                    success: function (result) {
+                        alert(result);
+                    },
+                    failure: function (errMsg) {
+                        alert(errMsg);
+                    }
+                });
+            }  else {
+                alert ("Please enter a valid email!");
+            }
         },
 
         getEmailObject: function () {
@@ -199,9 +200,14 @@ define([
             emailObject.email = this.emailEmailEl.val();
             emailObject.message = this.emailMessageEl.val();
             return emailObject;
+        },
+
+        isEmailValid: function (email) {
+            console.log(email);
+            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+            return pattern.test(email);
         }
 
-    })
-        ;
+    });
 })
 ;
